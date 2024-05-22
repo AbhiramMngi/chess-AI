6e6b1f98bf3ecd55aa55ba5fb6b0ac48a831c540
Abhiram Reddy <abhiramngi@Abhirams-MacBook-Air.local>
2024-05-22 17:39:55 +0530

    Add  AlphaZero model

--- engine/engine.py
+++ engine/engine.py
@@ -5,14 +5,16 @@ import numpy as np
 
 class ChessEngine:
   def __init__(self, config={}, depth=10):
-    self.engine = Stockfish(os.environ["STOCKFISH_PATH"], parameters=config, depth=depth)
+    self.engine = Stockfish(os.environ["STOCKFISH_ENGINE_PATH"], parameters=config, depth=depth)
   
   def set_position(self, position):
     self.engine.set_fen_position(position)
   
   
   def evaluate(self, state):
-    eval = self.engine.set_fen_position(str(state)).get_evaluation()
+    print("fuckk")
+    self.engine.set_fen_position(str(state))
+    eval = self.engine.get_evaluation()
 
     return eval["value"]
 
